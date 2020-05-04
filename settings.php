eae93df086d80ab5a35c8d6ff979154204bb12e3
Евгенюк <babin.jenya@yandex.ru>
2020-05-04 15:27:15 +0300

    Микро-обновление

--- settings.php
+++ settings.php
@@ -1,8 +1,8 @@
 <?php
 	$token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; //Токен группы
-																									  //Пример: http://prntscr.com/sanpe4
+	//Пример: http://prntscr.com/sanpe4
 	$secretKey = "zabanimpidorov"; //Секретный код для подключения CallBack API (Вводится в адрес. строку)
-								   //Пример: http://127.0.0.1/kick/get.php?akey=zabanimpidorov
+	//Пример: http://127.0.0.1/kick/get.php?akey=zabanimpidorov
 	$confirmationKey = "XXXXXXXX"; //Секретный код для подключения CallBack API (Выдаёт VK)
-								   //Пример: http://prntscr.com/sanoex
+	//Пример: http://prntscr.com/sanoex
 ?>
