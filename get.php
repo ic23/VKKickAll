f5ec465f88e5c0b0cff94eb3871308d4c3534f8c
Евгенюк <babin.jenya@yandex.ru>
2024-03-21 02:08:02 +0300

    Коммит ластовой версии скрипта
    Есть пара улучшений и сделана замена проверки фраз для кика на регулярное выражение.

--- get.php
+++ get.php
@@ -14,18 +14,32 @@
 
 	include("settings.php");
 	
-	function kick($usr_id, $idconserv) {
-		global $token;
-		$idconserv = $idconserv - 2000000000;
-		$eeee = file_get_contents("https://api.vk.com/method/messages.removeChatUser?access_token={$token}&chat_id={$idconserv}&user_id={$usr_id}&v=5.88", true);
-	}
-	
 	$keyGet = $_REQUEST["akey"];
 	$userdata = json_decode(file_get_contents('php://input'));
 	
+	function kick($usr_id, $idconserv) {
+		$idconserv = $idconserv - 2000000000;
+		$eeee = file_get_contents("https://api.vk.com/method/messages.removeChatUser?access_token={$GLOBALS['token']}&chat_id={$idconserv}&user_id={$usr_id}&v=5.88", true);
+	}
+	
+	function snd_msg($idconserv,$msg) {
+		$nernd_id = $GLOBALS['userdata']->object->date;
+		$nernd_id .= $GLOBALS['userdata']->object->from_id;
+		$request_params = array( 
+			'message' => $msg,
+			'random_id' => $nernd_id,
+			'peer_id' => $idconserv, 
+			'access_token' => $GLOBALS['token'], 
+			'v' => '5.103' 
+		);
+		$get_params = http_build_query($request_params); 
+		$otvet = file_get_contents('https://api.vk.com/method/messages.send?'. $get_params);
+	}
+	
 	if($keyGet == $secretKey)
 	{
-		switch ($userdata->type) {
+		switch ($userdata->type) 
+		{
 			case 'confirmation':
 				die($confirmationKey);
 				break;
@@ -34,26 +48,98 @@
 				$message = mb_strtolower($message, 'UTF-8');
 				$id = $userdata->object->from_id;
 				$beseda = $userdata->object->peer_id;
-				if (strpos($message, '@all') !== false) 
-				{
-					kick($id, $beseda);
-					die("ok");
-					break;
-				}
-				elseif (strpos($message, '@everyone') !== false)
-				{
-					kick($id, $beseda);
-					die("ok");
-					break;
-				}
-				elseif (strpos($message, '@online') !== false)
-				{
-					kick($id, $beseda);
+				if ($id == $beseda) {
+					snd_msg($beseda, $GLOBALS['privatemessage']);
 					die("ok");
 					break;
 				} else {
+					if (strpos($message, $GLOBALS['testcmd']) !== false) {
+						snd_msg($beseda, $GLOBALS['testanswer']);
+					}
+					
+					if ($message == "") {
+						$json = json_decode(file_get_contents('php://input'),1);
+						$action = $json[object]['action'];
+						if (isset($action['type'])) {
+							$member_id = $action['member_id'];
+							if ($action['type'] == "chat_invite_user") {
+								if ($id == $member_id) {
+									snd_msg($beseda, $GLOBALS['returnconserv']);
+									die("ok");
+									break;
+								} else {
+									snd_msg($beseda, $GLOBALS['greeting']);
+									die("ok");
+									break;
+								}
+							} elseif ($action['type'] == "chat_invite_user_by_link") {
+								snd_msg($beseda, $GLOBALS['greeting']);
+								die("ok");
+								break;
+							} elseif ($action['type'] == "chat_kick_user") {
+								if ($id == $member_id) {
+									snd_msg($beseda, $GLOBALS['userleave']);
+								} else {
+									snd_msg($beseda, $GLOBALS['kickadminpanel']);
+								}
+								die("ok");
+								break;
+							} else {
+								die("ok");
+								break;
+							}
+						}
+					}
+					
+					if (strpos($message, '!kick') !== false) {
+						json_decode(file_get_contents('php://input'));
+						$request_params = array( 
+						'peer_ids' => $beseda, 
+						'access_token' => $GLOBALS['token'], 
+						'v' => '5.103' 
+						);
+						$get_params = http_build_query($request_params); 
+						$otvet = file_get_contents('https://api.vk.com/method/messages.getConversationsById?'. $get_params);
+						$otvet = json_decode($otvet,1);
+						$admins[] = $otvet['response']['items']['0']['chat_settings']['owner_id'];
+						$admins = array_merge($admins,$otvet['response']['items']['0']['chat_settings']['admin_ids']);
+						if (in_array($id, $admins)) {
+							$json = json_decode(file_get_contents('php://input'),1);
+							if (isset($json[object]['fwd_messages']['0']['from_id'])) {
+								if (in_array($json[object]['fwd_messages']['0']['from_id'], $admins)) { 
+									$kostblLy = snd_msg($beseda, $GLOBALS['dontkickadmins']);
+								} else {
+									$kostblLy = snd_msg($beseda, $GLOBALS['byebye']);
+									kick($json[object]['fwd_messages']['0']['from_id'], $beseda);
+								}
+							} else {
+								$kostblLy = snd_msg($beseda, $GLOBALS['faggot']);
+							}
+						} else {
+							$kostblLy = snd_msg($beseda, $GLOBALS['youshallnotpass']);
+						}
+					}
+					
+					if (preg_match($GLOBALS['regular'], $message)) {
+						json_decode(file_get_contents('php://input'));
+						$request_params = array( 
+							'peer_ids' => $beseda, 
+							'access_token' => $GLOBALS['token'], 
+							'v' => '5.103' 
+						);
+						$get_params = http_build_query($request_params); 
+						$otvet = file_get_contents('https://api.vk.com/method/messages.getConversationsById?'. $get_params);
+						$otvet = json_decode($otvet,1);
+						$admins[] = $otvet['response']['items']['0']['chat_settings']['owner_id'];
+						$admins = array_merge($admins,$otvet['response']['items']['0']['chat_settings']['admin_ids']);
+						if (in_array($id, $admins)) {
+							$kostblLy = snd_msg($beseda, $GLOBALS['dontautokickadmins']);
+						} else {
+							$kostblLy = snd_msg($beseda, $GLOBALS['lastword']);
+							kick($id, $beseda);
+						}	
+					}
 					die("ok");
-					break;
 				}
 		}
 	}
